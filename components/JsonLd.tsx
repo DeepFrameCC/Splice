61df53143b2f9ee1bab3838edbb478adbfa2a4e5
tracy971 <tracy.dinga@etu.univ-orleans.fr>
2026-05-26 01:42:37 +0200

    fix: make JsonLd component synchronous to resolve Suspense rendering issue inside head

--- components/JsonLd.tsx
+++ components/JsonLd.tsx
@@ -1,14 +1,10 @@
-import { headers } from "next/headers";
-
 interface JsonLdProps {
   data: Record<string, unknown> | Record<string, unknown>[];
 }
 
-export default async function JsonLd({ data }: JsonLdProps) {
-  const nonce = (await headers()).get("x-nonce") ?? undefined;
+export default function JsonLd({ data }: JsonLdProps) {
   return (
     <script
-      nonce={nonce}
       type="application/ld+json"
       suppressHydrationWarning
       dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
